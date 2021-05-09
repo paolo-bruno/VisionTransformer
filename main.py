de52abfa5ce3df74affd3f31fe1d64e452c1b188
Paolo Bruno <brunopao97@gmail.com>
2021-05-09 15:36:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is an implementation of the VisionTransformer (ViT)
+# described in paper: https://arxiv.org/abs/2010.11929
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
